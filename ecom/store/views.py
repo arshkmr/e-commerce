1e3dc1b35b32ce524f41ce25b2f44b1545fdfbfd
arshkmr <arsh.com3@gmail.com>
2025-01-23 17:35:45 +0530

    add registration

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect
 from . models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
+
+
 
 # Create your views here.
 def home(reqeust):
@@ -28,9 +35,27 @@ def login_user(request):
         return render(request, 'login.html', {})
 
 
-
-
 def logout_user(request):
     logout(request)
     messages.success(request, ("You Have Been Logged Out...!!!"))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You Have Registered Successfully...WELCOME!!!"))
+            return redirect('home')
+        else:
+             messages.success(request, ("Oops! There was a Problem Registering, Please try again...!!!"))
+             return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
