8b82222380e0f644300acb1c659a6353fdc827e5
arshkmr <arsh.com3@gmail.com>
2025-01-24 19:40:14 +0530

    add product to cart

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -12,3 +12,14 @@ class Cart():
 
         # Make sure cart is available on all pages of sites
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -7,7 +11,26 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    # Get the Cart
+    cart = Cart(request)
+
+    # test for POST
+    if request.POST.get('action') == 'post':
+
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+
+        # lookup product in Database
+        product = get_object_or_404(Product, id = product_id)
+
+        # Save to Session
+        cart.add(product=product)
+
+        # return response
+        response = JsonResponse({'Product Name: ': product.name} )
+        return response
+
+
 
 
 def cart_delete(request):
